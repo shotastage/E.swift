5473240536bde5531f0bac1a5850449154060ff9
Shota Shimazu <shotastage@users.noreply.github.com>
2021-07-22 01:59:38 +0900

    revise

--- Sources/E.swift/main.swift
+++ Sources/E.swift/main.swift
@@ -1,8 +1,16 @@
 import ArgumentParser
 
 struct ESwiftMain: ParsableCommand {
+
+    @Argument()
+    var action: String
+
     mutating func run() throws {
         print("E.swift")
+
+        if action == "generate" {
+            print("API creation is not supported.")
+        }
     }
 }
 
